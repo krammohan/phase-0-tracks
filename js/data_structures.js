f09b79a0f8a6a51ea84e83da51d2d7133b97d9c6
Kavya Rammohan <kavyarammohan@Kavyas-MacBook-Pro.local>
2017-01-24 19:02:09 -0800

    Added car constructor and created car objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,3 +18,28 @@ for (i = 0; i < colors.length; i++) {
 
 // Constructor function for a car
 
+function Car(color, doors, isSportsCar) {
+  console.log("The car: ", this);
+  
+  this.color = color;
+  this.doors = doors;
+  this.isSportsCar = isSportsCar;
+  
+  this.honk = function(num) {
+    for (i = 0; i < num; i++){
+  	  console.log("Beep");
+  	}
+  	console.log("!");
+  }	
+
+}
+
+var car1 = new Car("red", 4, false);
+console.log(car1);
+console.log("Honking 5 times:");
+car1.honk(5);
+
+var car2 = new Car("blue", 2, true);
+console.log(car2);
+console.log("Honking 20 times:");
+car2.honk(20);
